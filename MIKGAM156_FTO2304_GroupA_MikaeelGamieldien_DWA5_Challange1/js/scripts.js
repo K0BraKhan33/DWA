ceed72a4e709d1a8935a313801c08618feb19168
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2023-08-23 15:28:14 +0200

    all

--- MIKGAM156_FTO2304_GroupA_MikaeelGamieldien_DWA5_Challange1/js/scripts.js
+++ MIKGAM156_FTO2304_GroupA_MikaeelGamieldien_DWA5_Challange1/js/scripts.js
@@ -14,50 +14,50 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 
-window.onerror = function(message, source, lineno, colno, error) {
+window.onerror = function (message, source, lineno, colno, error) {
   document.body.innerHTML = "Something critical went wrong. Please reload the page.";
   console.error("An error occurred:", message, "at", source, "Line:", lineno, "Column:", colno);
 };
 
-form.addEventListener("submit", (event) => {  
+form.addEventListener("submit", (event) => {
   event.preventDefault();
-  
+
 
 
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  
-  
-  
+
+
+
   try {
 
 
-//1
-  if (dividend === "" || divider === "") {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
-    return;
-  }
+    //1
+    if (dividend === "" || divider === "") {
+      result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+      return;
+    }
 
 
-  //2
-  if (isNaN(dividend) || isNaN(divider)) {
-    document.body.innerHTML = "Something critical went wrong. Please reload the page.";
-    throw new Error('Invalid input provided.');
-    return;
-  }
+    //2
+    if (isNaN(dividend) || isNaN(divider)) {
+      document.body.innerHTML = "Something critical went wrong. Please reload the page.";
+      throw new Error('Invalid input provided.');
+      return;
+    }
 
 
 
-//3
-  if (divider === "0") {
-    result.innerText = "Division not performed. Division by zero is not allowed.";
-    return;
-  }
-  
-  if (!Number.isInteger(+dividend) || !Number.isInteger(+divider)) {
-    result.innerText = "Division not performed. Both values should be whole numbers.";
-    return;
-  }
+    //3
+    if (divider === "0") {
+      result.innerText = "Division not performed. Division by zero is not allowed.";
+      return;
+    }
+
+    if (!Number.isInteger(+dividend) || !Number.isInteger(+divider)) {
+      result.innerText = "Division not performed. Both values should be whole numbers.";
+      return;
+    }
 
     const quotient = dividend / divider;
     if (!Number.isNaN(quotient) && Number.isInteger(quotient)) {
