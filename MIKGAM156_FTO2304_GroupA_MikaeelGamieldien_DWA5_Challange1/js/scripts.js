4f013f6123b6c1943b4fba5202ba7100c556e4ea
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2023-08-23 11:40:37 +0200

    all

---
+++ MIKGAM156_FTO2304_GroupA_MikaeelGamieldien_DWA5_Challange1/js/scripts.js
@@ -0,0 +1,60 @@
+// scripts.js
+
+/*
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  result.innerText = dividend / divider;
+}); */
+const form = document.querySelector("[data-form]");
+const result = document.querySelector("[data-result]");
+
+
+window.onerror = function(message, source, lineno, colno, error) {
+  document.body.innerHTML = "Something critical went wrong. Please reload the page.";
+  console.error("An error occurred:", message, "at", source, "Line:", lineno, "Column:", colno);
+};
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  const entries = new FormData(event.target);
+  const { dividend, divider } = Object.fromEntries(entries);
+  
+  if (dividend === "" || divider === "") {
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    return;
+  }
+  
+  if (isNaN(dividend) || isNaN(divider)) {
+    document.body.innerHTML = "Something critical went wrong. Please reload the page.";
+    console.error("Invalid input provided.");
+    return;
+  }
+  
+  if (divider === "0") {
+    result.innerText = "Division not performed. Division by zero is not allowed.";
+    return;
+  }
+  
+  if (!Number.isInteger(+dividend) || !Number.isInteger(+divider)) {
+    result.innerText = "Division not performed. Both values should be whole numbers.";
+    return;
+  }
+  
+  try {
+    const quotient = dividend / divider;
+    if (!Number.isNaN(quotient) && Number.isInteger(quotient)) {
+      result.innerText = quotient;
+    } else {
+      result.innerText = "Division not performed. Result is not a whole number.";
+    }
+  } catch (error) {
+    result.innerText = "Division not performed. An error occurred during division.";
+    console.error("An error occurred during division:", error);
+  }
+});
