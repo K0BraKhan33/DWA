3a1ede14143d1a0f0545c688071aa8dd0d94d09e
K0BraKhan33 <mikaeel.gamieldien@gmail.com>
2023-08-24 14:21:57 +0200

    all

--- MIKGAM156_FTO2304_GroupA_MikaeelGamieldien_DWA5_Challange1/js/scripts.js
+++ MIKGAM156_FTO2304_GroupA_MikaeelGamieldien_DWA5_Challange1/js/scripts.js
@@ -14,61 +14,49 @@ const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
 
-window.onerror = function (message, source, lineno, colno, error) {
-  document.body.innerHTML = "Something critical went wrong. Please reload the page.";
-  console.error("An error occurred:", message, "at", source, "Line:", lineno, "Column:", colno);
-};
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-
-
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-
+  console.clear;
 
 
   try {
 
 
-    //1
+    //2
     if (dividend === "" || divider === "") {
       result.innerText = "Division not performed. Both values are required in inputs. Try again.";
-      return;
-    }
-
-
-    //2
-    if (isNaN(dividend) || isNaN(divider)) {
-      document.body.innerHTML = "Something critical went wrong. Please reload the page.";
-      throw new Error('Invalid input provided.');
-      return;
-    }
-
+      throw new Error("Both values are required in inputs. Try again.")
+   }
 
 
     //3
-    if (divider === "0") {
+    if (isNaN(dividend) || isNaN(divider)) {
+      document.body.innerHTML = "Something critical went wrong. Please reload the page.";
+      throw new Error('Invalid input provided.');
+    }
+
+
+
+    //4
+    if (divider < "0") {
       result.innerText = "Division not performed. Division by zero is not allowed.";
-      return;
+      throw new Error('Division by zero is not allowed.')
     }
 
-    if (!Number.isInteger(+dividend) || !Number.isInteger(+divider)) {
-      result.innerText = "Division not performed. Both values should be whole numbers.";
-      return;
-    }
 
+    //1
     const quotient = dividend / divider;
     if (!Number.isNaN(quotient) && Number.isInteger(quotient)) {
       result.innerText = quotient;
     } else {
       result.innerText = "Division not performed. Result is not a whole number.";
-      throw new Error('')
+      throw new Error('Result is not a whole number.')
     }
   } catch (error) {
-    result.innerText = "Division not performed. An error occurred during division.";
-    console.error("An error occurred during division:", error);
+    console.error(error);
 
   }
 });
