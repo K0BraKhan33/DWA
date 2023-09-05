78f294a0bad325705ce5701fa801ca2ca9dd5424
K0BraKhan33 <mikaeel.gamieldien.gmail.com>
2023-09-05 10:49:47 +0200

    all

---
+++ MIKGAM156_FTO2304_GroupA_MikaeelGamieldien_DWA06 copy 2/book-connect-main/meta/js/book.js
@@ -0,0 +1,41 @@
+
+import { authors, BOOKS_PER_PAGE } from './data.js';
+
+class BookElementCreator {
+  constructor() {
+    this.booksPerPage = BOOKS_PER_PAGE;
+  }
+
+  createBookElement({ id, title, author, image }) {
+    const bookElement = document.createElement('button');
+    bookElement.classList.add('preview');
+    bookElement.setAttribute('data-preview', id);
+
+    bookElement.innerHTML = `
+      <img class="preview__image" src="${image}" />
+      <div class="preview__info">
+        <h3 class="preview__title">${title}</h3>
+        <div class="preview__author">${authors[author]}</div>
+      </div>
+    `;
+
+    return bookElement;
+  }
+
+  themes(LiDa) {
+    const root = document.documentElement;
+    if (LiDa === 'night') {
+      root.style.setProperty('--color-dark', '255, 255, 255');
+      root.style.setProperty('--color-light', '10, 10, 20');
+    } else {
+      root.style.setProperty('--color-dark', '10, 10, 20');
+      root.style.setProperty('--color-light', '255, 255, 255');
+    }
+  }
+}
+
+const bookElementCreator = new BookElementCreator();
+const createBookElement = bookElementCreator.createBookElement.bind(bookElementCreator);
+const themesnd = bookElementCreator.themes.bind(bookElementCreator);
+
+export { createBookElement, themesnd };
